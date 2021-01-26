16683ff657bca2ce01538c948426e372d0a3301d
Edgard Decena <edecena@gmail.com>
2021-01-26 19:35:58 -0400

    Corrección.

--- proyecto_1_calculo.py
+++ proyecto_1_calculo.py
@@ -56,3 +56,59 @@ def Lusandre_Marcano(conjunto):
     Recibe como parámetros un conjunto y retorna el máximo del conjunto.
     """
     return max(conjunto)
+
+if __name__ == "__main__":
+    # Testing ...
+
+    a = 1
+    b = 5
+    c = 7
+
+    lista    = [a, b, c]
+    tupla    = (a, b, c)
+    conjunto = {a, b, c}
+    cadena   = "Universidad de Oriente"
+
+    if Ashly_Scarpati(a, b, c) == min(a, b, c):
+        res = "10"
+    else:
+        res = "5"
+    res = 0
+    print("Ashly Scarpati:", res)
+
+    if Elder_Gomes(lista) == min(lista):
+        res = "10"
+    else:
+        res = "5"
+    res = 0
+    print("Elder Gomes:", res)
+
+    if Hector_Salazar(tupla) == tupla[0]:
+        res = "10"
+    else:
+        res = "5"
+    print("Hector Salazar:", res)
+
+    if Jose_Gomez(cadena) == len(cadena):
+        res = "10"
+    else:
+        res = "5"
+    print("Jose Gomez:", res)
+
+    if Lewis_Narvaez(tupla) == tupla[-1]:
+        res = "10"
+    else:
+        res = "5"
+    print("Lewis Narvaez:", res)
+
+    if Luisana_Hernandez(a, b, c) == max(a, b, c):
+        res = "10"
+    else:
+        res = "5"
+    print("Luisana Hernandez:", res)
+
+    if Lusandre_Marcano(conjunto) == max(conjunto):
+        res = "10"
+    else:
+        res = "5"
+    print("Lusandre Marcano:", res)
